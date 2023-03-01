741113246c2e4e2a650ef970e8aa891ed65afe9e
Esteban <caguirreacevedo@hotmail.com>
2023-03-01 16:21:49 -0600

    xd

--- Olmecas-front/src/WorkSpaces/Esteban.jsx
+++ Olmecas-front/src/WorkSpaces/Esteban.jsx
@@ -18,11 +18,21 @@ export const Esteban = () => {
   const basic_auth = `${user}:${password}`
 
   fetch("https://54.145.241.208:55000/security/user/authenticate", {
-    mode: 'no-cors',
     headers: auth
   })
     .then(response => console.log(response))
     .then(data => console.log(data));
+
+  /* const token = "eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ3YXp1aCIsImF1ZCI6IldhenVoIEFQSSBSRVNUIiwibmJmIjoxNjc3NzA4ODcwLCJleHAiOjE2Nzc3MDk3NzAsInN1YiI6IndhenVoLXd1aSIsInJ1bl9hcyI6ZmFsc2UsInJiYWNfcm9sZXMiOlsxXSwicmJhY19tb2RlIjoid2hpdGUifQ.AY3IixR6nAmFoLgPlvT2yXgiIckHjykgjR0CTGvwM8LGAh528l0rBfKDtioU26ZW_kFW8ivaIQzKuZ0PlrkkVeCEAVq8vPuHr5RB0oRV87cp8mHkfPeyaWCKEdmwhTT8hnoD6o8W386IxSFBv3frcV8_KNpwUU-zP16zq9KP0e3Iv-CS"
+
+  fetch("https://54.145.241.208:55000/agents", {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+    .then(response => console.log(response))
+    .then(data => console.log(data)); */
 
   return <div>Esteban</div>;
 };
