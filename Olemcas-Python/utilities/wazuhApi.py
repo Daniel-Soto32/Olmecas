6467f6f033803c83a9155611bbc393e3b888cebe
Esteban <caguirreacevedo@hotmail.com>
2023-03-04 01:09:34 -0600

    Update wazuhApi.py

--- Olemcas-Python/utilities/wazuhApi.py
+++ Olemcas-Python/utilities/wazuhApi.py
@@ -3,6 +3,8 @@ from base64 import b64encode
 
 import requests  # To install requests, use: pip install requests
 import urllib3
+import pandas as pd
+import numpy as np
 
 class apiHandler:
     
@@ -20,6 +22,9 @@ class apiHandler:
     base_url = f"{protocol}://{host}:{port}"
     login_url = f"{base_url}/security/user/authenticate"
     basic_auth = f"{user}:{password}".encode()
+    
+    #jsonApi
+    jsonApi = None
     
     headers = {}
     allAgents = None
@@ -60,7 +65,13 @@ class apiHandler:
     def get_agents(self):
         url = self.base_url + '/agents'
         self.allAgents = self.get_response(url, self.headers)
-        return self.allAgents
+        self.jsonApi = self.allAgents['data']
+        return self.jsonApi
+    
+    def get_vul(self, agent_id):
+        url = self.base_url + "/vulnerability/" + agent_id
+        return self.get_response(url, self.headers)['data']
+        
     
     def get_vul_by_crit(self, severity):
         total_vulnerabilities = 0
@@ -68,10 +79,10 @@ class apiHandler:
         url_end = "/summary/severity"
         url_mid = ""
         
-        for i in range(1, jsonApi["data"]["total_affected_items"]):
-            url_mid = jsonApi["data"]["affected_items"][i]["id"]
+        for i in range(1, self.jsonApi["total_affected_items"]):
+            url_mid = self.jsonApi["affected_items"][i]["id"]
             url = self.base_url + url_start + url_mid + url_end
-            num_crit = self.get_response(url, self.headers)['data']['severity'].get(severity)
+            num_crit = self.get_response(url, self.headers)['severity'].get(severity)
             if( num_crit is not None):
                 total_vulnerabilities += num_crit
         return total_vulnerabilities
@@ -80,14 +91,22 @@ class apiHandler:
         url_start = "/vulnerability/"
         url_end = f"?pretty=true&search={keyWord}&select=name,condition,status,severity,cve&limit={limite}"
         url_mid = ""
+        all_responses = {}
         
-        for i in range(1, jsonApi["data"]["total_affected_items"]):
-            url_mid = jsonApi["data"]["affected_items"][i]["id"]
+        for i in range(1, self.jsonApi["data"]["total_affected_items"]):
+            url_mid = self.jsonApi["data"]["affected_items"][i]["id"]
             url = self.base_url + url_start + url_mid + url_end
             
-            response = self.get_response(url, self.headers)['data']
-            print(response)
+            response = self.get_response(url, self.headers)['data']['affected_items']
+            if( len(response) > 0 ):
+                all_responses[url_mid] = {
+                    'device_name': self.jsonApi["data"]["affected_items"][i]["name"],
+                    'vulnerabilities': response
+                    }
+            
+        return all_responses
         
+    ''' ________________________ Pending testing ________________________ '''
     def upgrade_agents(self, agents):
         url = self.base_url + "/agents/upgrade?agents_list=" + agents
         response = self.put_response(url, self.headers)['data']
@@ -102,6 +121,27 @@ class apiHandler:
         url = self.base_url + "/agents?agents_list=" + agents + '&status=' + status + '&older_than=' + older_than
         response = self.put_response(url, self.headers)['data']
         return response
+    
+    def get_common_agents(self, keyWord, limite = 100):
+        response = self.get_vul_by_key(keyWord, limite)
+        return response.keys()
+                
+    def get_top_10_vul(self):
+        all_agents_vul = {}
+        agents = self.jsonApi["affected_items"][1:]
+        for agent in agents:
+            agent_vul = self.get_vul(agent['id'])
+            for vuls in agent_vul.values():
+                print(vuls)
+                for vul in vuls:
+                    if (vul['cve'] not in all_agents_vul):
+                        all_agents_vul[vul['cve']] = 1
+                    else:
+                        all_agents_vul[vul['cve']] += 1
+        
+        print(all_agents_vul)
+            
+        
 
 apiTest = apiHandler()
 
@@ -123,6 +163,18 @@ jsonApi = apiTest.get_agents()
 
 ''' 3) '''
 #print(apiTest.upgrade_agents('001,002'))
+#print(apiTest.restart_agents('001,002'))
+#STATUS AVAILABLES: "all" "active" "pending" "never_connected" "disconnected"
+#print(apiTest.delete_agents('001,002', "disconnected))
 
+''' 4) '''
+#print(apiTest.get_common_agents("Windows"))
 
-print(response)
+''' 5) '''
+print(apiTest.get_top_10_vul())
+
+''' 6) '''
+
+''' 7) '''
+
+''' 8) '''
